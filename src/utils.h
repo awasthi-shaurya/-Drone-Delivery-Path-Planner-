4f8e24536a549f3c84621883702c3b765cb184df
awasthi-shaurya <25lbcs3197@culkomail.in>
2025-11-30 19:02:33 +0530

    Updated utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,1 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "planner.h"
+
+int count_pending(Package *p, int n);
+double compute_fuel(double dist, double payload, double baseFuel, double weightFactor);
+void print_final_report(Package *p, int n, double dist, double fuel, double time);
+
+#endif
 
