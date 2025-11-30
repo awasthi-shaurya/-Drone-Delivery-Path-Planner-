aa3ca809839f9c95dcb823348e41db767b794ae3
Aniket <25lbcs3174@culkomail.in>
2025-11-30 21:26:18 +0530

    Add Package struct and delivery planning functions

--- src/planner.h
+++ src/planner.h
@@ -1,1 +1,18 @@
+#ifndef PLANNER_H
+#define PLANNER_H
 
+typedef struct {
+    int id;
+    double x, y;
+    double weight;
+    int priority;
+    int delivered;
+    int undeliverable;
+} Package;
+
+void plan_deliveries(Package *packages, int n, double capacity,
+                     double speed, double baseFuel, double weightFactor);
+
+int choose_next(Package *packages, int n, double cx, double cy, double remaining);
+
+#endif
